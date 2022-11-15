19ed763148b9a396d963d34a25696837150d780b
ecatto <ecatto@aol.com>
2022-11-15 13:37:40 +0000

    test

--- src/main/java/org/solent/ood/hittastic/Song.java
+++ src/main/java/org/solent/ood/hittastic/Song.java
@@ -8,6 +8,45 @@ package org.solent.ood.hittastic;
  *
  * @author 44774
  */
-public class Song {
+public class Song 
+{
+    //Attributes
+    private String artist, title;
+    private int quantitySold;
     
+    //Constructor
+    public Song(String artistIn, String titleIn)            
+    {
+        artist = artistIn;
+        title = titleIn;
+        quantitySold = 0;
+    }
+    
+    //Getter methods
+    public String getArtist()
+    {
+        return artist;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+    
+    public int getQuantitySold()
+    {
+        return quantitySold;
+    }
+        
+    //Other methods
+    public void addQuantitySold(int quantityIn)
+    {
+            quantitySold = quantitySold + quantityIn;
+    }
+    
+
+    public void display() 
+    {
+        System.out.println("Artist: " + artist + ", Song Title: " + title);
+    }
 }
