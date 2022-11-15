b5c368b65236811d7f1b3207863ac11312a697d3
ecatto <ecatto@aol.com>
2022-11-15 14:22:34 +0000

    complete

--- src/main/java/org/solent/ood/hittastic/HitSite.java
+++ src/main/java/org/solent/ood/hittastic/HitSite.java
@@ -4,10 +4,158 @@
  */
 package org.solent.ood.hittastic;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author 44774
  */
-public class HitSite {
+public class HitSite 
+{    
+    //Attributes
+    private ArrayList<Song> songs;
+    private ArrayList<Order> orders;
     
+    //constructor
+    public HitSite()
+    {
+        songs = new ArrayList<Song>();
+        orders = new ArrayList<Order>();
+    }
+
+    //methods
+    
+    //add a song to the songs ArrayList
+    public void addSong(Song s)
+    {
+        songs.add(s);
+    }
+    
+    
+    //Searches through all songs. Add all songs by the artist passed in to an ArrayList Retuens that ArrayList
+    public ArrayList<Song> findSongsByArtist(String artist)
+    {
+        ArrayList<Song> foundArtist = new ArrayList<>();
+        
+        for(Song currentSong: songs)
+        {
+            if(currentSong.getArtist().equals(artist))
+            {
+                foundArtist.add(currentSong);
+            }
+        }
+        return foundArtist;               
+    }
+    
+    //Searches through all songs. Add all songs by the title passed in to an ArrayList Retuens that ArrayList    
+    public ArrayList<Song> findSongsByTitle(String title)
+    {
+        ArrayList<Song> foundTitle = new ArrayList<>();
+
+        for(Song currentSong: songs)
+        {
+            if(currentSong.getTitle().equals(title))
+            {
+                foundTitle.add(currentSong);
+            }
+        }
+        return foundTitle;        
+    }
+
+    //ArrayList of songs by title passed in. If is more than one, get artist info from user
+    //If no artist matches user input, repeat until artist matches 
+    //Return song which has matching title and artist 
+    public Song findSongByTitleAndArtist(ArrayList<Song> titles)
+    {
+        if (titles.size() >1)
+        {                        
+            boolean found = false;
+                        
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("There is more than 1 song with this title");
+            while (found == false)
+            {
+                System.out.println("Please enter the artist: ");
+                String searchArtist = scanner.nextLine();
+                          
+                for(Song currentSong: titles)
+                {
+                    if(currentSong.getArtist().equals(searchArtist))
+                    {
+                        found = true;
+                        return currentSong;
+                    }
+                }
+                
+                if (found == true)
+                {
+                    break;
+                }    
+                else
+                {
+                    System.out.println("Sorry, no artists match that song title, please try again");      
+                }                
+            }
+        }
+                    
+        else
+        {
+            for(Song currentSong: titles)
+            {
+                return currentSong;
+            }                    
+        }
+        return null;
+    }
+
+    //get quantity and check that is more than zero       
+    public int getQuantity()
+    {               
+        Scanner scanner = new Scanner(System.in);
+        
+        boolean validQuanity = false;                    
+        while (validQuanity == false)
+        {                    
+            System.out.println("Enter the quantity you would like to purchase:");
+            String input = scanner.nextLine();   
+            int quantity = Integer.parseInt(input);
+                        
+            if (quantity >0)
+            {
+                validQuanity = true;
+                return quantity;
+            }
+            else
+            {
+                System.out.println("Quantity must be more than zero");
+            }
+        }
+        return 0;
+    }
+
+    //create new order and add to arraylist of all orders
+    //update quantity sold        
+    public void buySong(int ordNum, String custId, Song songIn, int quantity)
+    {
+        orders.add(new Order(ordNum, custId, songIn, quantity));
+        songIn.addQuantitySold(quantity);       
+    }
+    
+    
+    //Display all orders for specified customer
+    public ArrayList<Order> viewOrders(String custId)
+    {
+        ArrayList<Order> custOrders = new ArrayList<>();
+        
+        for (Order currentOrder: orders)
+        {    
+            if(currentOrder.getCustId().equals(custId))
+            {
+                custOrders.add(currentOrder);
+            }
+        }
+        return custOrders;
+    }    
 }
+
--- src/main/java/org/solent/ood/hittastic/HitTastic.java
+++ src/main/java/org/solent/ood/hittastic/HitTastic.java
@@ -4,14 +4,150 @@
  */
 
 package org.solent.ood.hittastic;
-
+import java.util.Scanner;
+import java.util.ArrayList;
 /**
- *
- * @author 44774
+ *-
+ * @author Ellie Catto
  */
-public class HitTastic {
+public class HitTastic 
+{
+    
+    public static void main(String[] args) 
+    {            
+        HitSite wibble = new HitSite();
+        wibble.addSong(new Song("Taylor Swift", "You belong with me"));
+        wibble.addSong(new Song("Taylor Swift", "Red"));
+        wibble.addSong(new Song("Queen", "Radio Gaga"));    
+        wibble.addSong(new Song("Simply Red", "Red"));  
+        
+    //Once integrated the customer will be identified from an ArrayList when they log in    
+        Customer example = new Customer("1234", "Ellie");
+        
+        Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String choice = "";
+
+        int orderNumber = 1;
+        
+        // Loop until the option equals "5" (quit)
+        while(! choice.equals("5"))
+        {
+            System.out.println(" ");
+            System.out.println("1. Search for songs by artist");
+            System.out.println("2. Search for songs by title");
+            System.out.println("3. Purchase song");
+            System.out.println("4. View order history");            
+            System.out.println("5. Quit Program");
+
+            System.out.println("Please enter a number: ");
+
+            choice = scanner.nextLine();
+
+            switch(choice)
+            {
+                // Search for songs by artist
+                case "1":
+                    System.out.println("Please enter the name of the artist:");
+                    String artistName = scanner.nextLine();                                                          
+                    
+                     ArrayList<Song> foundArtist = wibble.findSongsByArtist(artistName);                    
+                    // If no matches, the array list will be empty
+                    if(foundArtist.isEmpty())
+                    {
+                        System.out.println("Sorry, no artists with that name exist");
+                    }
+                    else
+                    {
+                        // Loop through all songs by artist
+                        // Add songs to the ArrayList                        
+                        for(Song s: foundArtist)
+                        {
+                            s.display();
+                        }
+                    }             
+                    break;
+
+                // Search for songs by title
+                case "2":
+                    System.out.println("Please enter the title of the song:");
+                    String titleName = scanner.nextLine();                                                          
+                    
+                     ArrayList<Song> foundTitle = wibble.findSongsByTitle(titleName);                    
+                    // If no matches, the array list will be empty
+                    if(foundTitle.isEmpty())
+                    {
+                        System.out.println("Sorry, no songs with that title exist");
+                    }
+                    else
+                    {
+                        // Loop through all songs by title
+                        // Add songs to the ArrayList                        
+                        for(Song s: foundTitle)
+                        {
+                            s.display();
+                        }
+                    }                    
+                    break;
+
+                // Purchase song
+                    //find song wanted
+                    //ask quantity
+                    //call HitSite.buySong
+                case "3":
+                    System.out.println("Enter the title of the song you would like to purchase:");
+                    String searchName = scanner.nextLine();
+
+                    foundTitle = wibble.findSongsByTitle(searchName);                    
+                    // If no matches, the array list will be empty
+                    if(foundTitle.isEmpty())
+                    {
+                        System.out.println("Sorry, no songs with that title exist");
+                        break;
+                    }
+                    else
+                    {                        
+                        // Loop through all songs by title
+                        // Add songs to the ArrayList
+                        
+                        for(Song s: foundTitle)
+                        {
+                            s.display();
+                        }
+                    }
+                    Song foundSong = wibble.findSongByTitleAndArtist(foundTitle);    
+
+                    int quantity = wibble.getQuantity();
+                                        
+                    wibble.buySong(orderNumber, example.getCustId(), foundSong, quantity);
+                    orderNumber++;
+                    if (quantity ==1)
+                    {
+                        System.out.println("You have bought 1 copy of " + foundSong.getTitle() + " by " + foundSong.getArtist() );                        
+                    } 
+                    else
+                    {
+                        System.out.println("You have bought " + quantity + " copies of " + foundSong.getTitle() + " by " + foundSong.getArtist() );                            
+                    }
+                    
+                    break;
+                    
+                // View order history
+                case "4":                    
+                    ArrayList<Order> orders = wibble.viewOrders(example.getCustId());
+                    if (orders.isEmpty())
+                    {
+                        System.out.println("You have no orders to display");              
+                    }
+                    else
+                    {
+                        for(Order currentOrder: orders)
+                        {
+                            currentOrder.display();
+                        }
+                    }
+                    break;                
+            }
+        }                
     }
 }
