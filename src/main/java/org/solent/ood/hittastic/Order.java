e412d258507de253f7a79ed84afb4b249921a9bc
ecatto <ecatto@aol.com>
2022-11-15 14:17:21 +0000

    test2

--- src/main/java/org/solent/ood/hittastic/Order.java
+++ src/main/java/org/solent/ood/hittastic/Order.java
@@ -8,6 +8,47 @@ package org.solent.ood.hittastic;
  *
  * @author 44774
  */
-public class Order {
+public class Order 
+{
+    //Attributes
+    private String custId;
+    private int orderNum, quantity;
+    private Song songInOrder;
     
+    //Constructor
+    public Order(int orderNumIn, String custIdIn, Song songInOrderIn, int quantityIn)            
+    {
+        orderNum = orderNumIn;
+        custId = custIdIn;
+        songInOrder = songInOrderIn;
+        quantity = quantityIn;
+    }
+    
+    //Getter mthods
+    public String getCustId()
+    {
+        return custId;
+    }
+    
+    public int getorderNum()
+    {
+        return orderNum;
+    }
+
+    public int getQuantity()
+    {
+        return quantity;
+    }
+
+    //Setter method
+    public Song getSongInOrder()
+    {
+        return songInOrder;
+    }
+    
+    //Other methods
+    public void display()
+    {
+        System.out.println("Order Number: " + orderNum + ". " + quantity + " copy/copies of " + getSongInOrder().getTitle() + " by " + getSongInOrder().getArtist());
+    }
 }
